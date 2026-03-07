c72f151643cdf8cc68b6917c9adeade100bd1212
Ddumb-new <davidhyolmo529@gmail.com>
2026-03-07 14:51:04 -0500

    Added forms and h1 tags to login and register pages

---
+++ LAB04/sushiapp/app/components/Register.js
@@ -0,0 +1,21 @@
+export default function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+
+      <form>
+        <input type="text" placeholder="Username" />
+        <br />
+
+        <input type="email" placeholder="Email" />
+        <br />
+
+        <input type="password" placeholder="Password" />
+        <br />
+
+        <button type="submit">Register</button>
+      </form>
+
+    </div>
+  );
+}
