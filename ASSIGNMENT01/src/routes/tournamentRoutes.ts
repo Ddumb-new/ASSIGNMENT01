9a14aa08c47490f542591f791d5a3f6a2eb659a9
Ddumb-new <davidhyolmo529@gmail.com>
2026-03-30 00:17:59 -0400

    Player CRUD
    Implemented full CRUD operations for player including populate for related data

--- ASSIGNMENT01/src/routes/tournamentRoutes.ts
+++ ASSIGNMENT01/src/routes/tournamentRoutes.ts
@@ -1,8 +1,8 @@
 import { Router, Request, Response } from "express";
 import Tournament from "../models/tournament";
-
+import Player from "../models/player";
+import mongoose from "mongoose";
 const router = Router();
-
 // GET all tournaments
 router.get("/", async (req: Request, res: Response) => {
   try {
@@ -21,6 +21,27 @@ router.post("/", async (req: Request, res: Response) => {
     res.status(201).json(savedTournament);
   } catch (error) {
     res.status(500).json({ error: "Failed to create tournament" });
+  }
+});
+
+// GET all players for a specific tournament
+router.get("/:id/players", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id as string;
+
+      console.log("👉 Requested ID:", id);
+
+  const allPlayers = await Player.find();
+  console.log("👉 ALL PLAYERS:", allPlayers);
+
+    const players = await Player.find({
+      tournamentId: id,
+    });
+    console.log("Filtered Players",players);
+
+    res.status(200).json(players);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch players for tournament" });
   }
 });
 
